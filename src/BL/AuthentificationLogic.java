d3f118583d7b3434c6009fedf1265de0760af624
Prisca <prisca.relique@gmail.com>
2015-03-03 11:05:53 +0100

    Modifs AuthentificationLogic et Database
    AuthentificationLogic implemente IUser
    Database doit implementer une fonction pour verifier l’id et le
    motdepasse

--- src/BL/AuthentificationLogic.java
+++ src/BL/AuthentificationLogic.java
@@ -7,11 +7,38 @@ import BL.IUserStore;
  * 
  * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7e20]
  */
-public class AuthentificationLogic {
+public class AuthentificationLogic implements IUser {
 /**
  * 
  * 
  * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7e07]
  */
     public BL.IUser iUser;
+    private Data.DataBase database;
+    
+
+
+@Override
+public Boolean checkCredentials(String username, String password) {
+	// TODO Auto-generated method stub
+	return database.checkCredentials(username,password);
+}
+
+@Override
+public Boolean changePassword(String username, String oldPass, String newPass) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public String createUser(String username) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public void deleteUser(String username) {
+	// TODO Auto-generated method stub
+	
+}
  }
--- src/Data/DataBase.java
+++ src/Data/DataBase.java
@@ -13,4 +13,14 @@ public class DataBase {
  * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7d86]
  */
     public BL.Connection connection;
+    
+    
+    
+    //Check id and password in the database
+    public Boolean checkCredentials(String username, String password){
+    	
+    	
+    }
  }
+
+
