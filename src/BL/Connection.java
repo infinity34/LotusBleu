f7091e55abeb6a0fac1a55297ceea4294c7060cf
clerixmaxime <solidsnake444@hotmail.fr>
2015-03-05 15:15:28 +0100

    Connection au propre ma gueule

--- src/BL/Connection.java
+++ src/BL/Connection.java
@@ -1,66 +1,48 @@
-
 /*Class Must be Singleton*/
 package BL;
-<<<<<<< HEAD
 
 import java.sql.*;
 
-=======
-import java.sql.*;
->>>>>>> origin/conception_login
 /**
  * 
  * 
  * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7dc5]
  */
 public class Connection {
-/**
- * 
- * 
- * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7d83]
- */
-    public Data.DataBase dataBase;
-<<<<<<< HEAD
-    
-    public static void main(String[] args) {
-    	  try {
-    	    Class.forName("com.mysql.jdbc.Driver");
-    	       
-    	    String url = "jdbc:mysql://lotusbleu.cveov0u0zsel.us-west-2.rds.amazonaws.com:3306/lotusbleu";
-    	    String user = "lotusbleu";
-    	    String passwd = "lotusbleu";
-    	       
-    	    System.out.println("J'essaye de me co");
-    	    java.sql.Connection con = DriverManager.getConnection(url, user, passwd);
-	        System.out.println("Je suis co");
-    	    System.out.println(con.getClientInfo());
-    	       
-    	  } catch (Exception e) {
-    	    e.printStackTrace();
-    	  }      
-    	}
-=======
-    private Statement state;
-    private Connection con;
-    private String url;
-    private String user;
-    private String password;
-	private void Connection(String url, String user, String password) {
-		this.url = url;
-		this.user = user;
-		this.password = password;
-		try{
-			this.con = (Connection) DriverManager.getConnection(this.url, this.user, this.password); //DriverManager from Java.Connection
+	/**
+	 * 
+	 * 
+	 * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7d83]
+	 */
+	public Data.DataBase dataBase;
+	private Statement state;
+	private java.sql.Connection con;
+	
+	final static String url = "jdbc:mysql://lotusbleu.cveov0u0zsel.us-west-2.rds.amazonaws.com:3306/lotusbleu";
+	final static String user = "lotusbleu";
+	final static String password = "lotusbleu";
+
+	public static void main(String[] args) {
+		Connection con = new Connection();
+	}
+
+	public Connection() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.con = DriverManager.getConnection(url,
+					user, password);
+			System.out.println("Je suis connecté");
 			this.state = ((java.sql.Connection) this.con).createStatement();
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace(); // Just for the moment, we had to think about how to manga it later
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+
 	public Statement getState() {
-		// TODO Auto-generated method stub
 		return this.state;
 	}
->>>>>>> origin/conception_login
- }
+}
