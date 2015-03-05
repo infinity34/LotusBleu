f1c962612a99ff0011eb2e1c07e0e21cbf9cd00b
clement roque <clement.eberoque@gmail.com>
2015-03-05 14:56:27 +0100

    Classe Connection à Amazon ok

--- src/BL/Connection.java
+++ src/BL/Connection.java
@@ -1,7 +1,13 @@
 
 /*Class Must be Singleton*/
 package BL;
+<<<<<<< HEAD
+
 import java.sql.*;
+
+=======
+import java.sql.*;
+>>>>>>> origin/conception_login
 /**
  * 
  * 
@@ -14,6 +20,26 @@ public class Connection {
  * @poseidon-object-id [Im7393253bm14bd0a52e7dmm7d83]
  */
     public Data.DataBase dataBase;
+<<<<<<< HEAD
+    
+    public static void main(String[] args) {
+    	  try {
+    	    Class.forName("com.mysql.jdbc.Driver");
+    	       
+    	    String url = "jdbc:mysql://lotusbleu.cveov0u0zsel.us-west-2.rds.amazonaws.com:3306/lotusbleu";
+    	    String user = "lotusbleu";
+    	    String passwd = "lotusbleu";
+    	       
+    	    System.out.println("J'essaye de me co");
+    	    java.sql.Connection con = DriverManager.getConnection(url, user, passwd);
+	        System.out.println("Je suis co");
+    	    System.out.println(con.getClientInfo());
+    	       
+    	  } catch (Exception e) {
+    	    e.printStackTrace();
+    	  }      
+    	}
+=======
     private Statement state;
     private Connection con;
     private String url;
@@ -36,4 +62,5 @@ public class Connection {
 		// TODO Auto-generated method stub
 		return this.state;
 	}
+>>>>>>> origin/conception_login
  }
