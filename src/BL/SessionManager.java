315120bf14c6dfb3a861c13f3032bd339e467f19
infinity34 <remy@162.38.33.163>
2015-03-06 09:57:51 +0100

    ajout paramètres dans la fonction login de SessionManager

--- src/BL/SessionManager.java
+++ src/BL/SessionManager.java
@@ -7,41 +7,42 @@ package BL;
  */
 public class SessionManager {
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean Login() {        
-        // your code here
-        return null;
-    } 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean Logout() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Boolean Login(String username, String password) {        
+		// your code here
+		return null;
+	} 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Data.User GetCurrentUser() {        
-        // your code here
-        return null;
-    } 
-/**
- * 
- * 
- */
-    public Data.User user;
- }
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Boolean Logout() {        
+		// your code here
+		return null;
+	} 
+
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Data.User GetCurrentUser() {        
+		// your code here
+		return null;
+	} 
+	/**
+	 * 
+	 * 
+	 */
+	public Data.User user;
+}
