d73ef26804d15a785f169ed385b5822536b319c4
infinity34 <remy.tartiere@free.fr>
2015-03-06 09:56:26 +0100

    creation class de test pour SessionManager

---
+++ src/Test/BL/SessionManagerDBTest.java
@@ -0,0 +1,37 @@
+package Test.BL;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import BL.SessionManagerDB;
+
+public class SessionManagerDBTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		SessionManagerDB sessionManagerDB = new SessionManagerDB();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
