77b4dabf2d5ae923a10a6fcb7afed74d8555fd77
infinity34 <remy@162.38.33.163>
2015-03-06 08:55:42 +0100

    creation constructeur debut affichage requete debut requete insertion

--- src/BL/SessionManagerDB.java
+++ src/BL/SessionManagerDB.java
@@ -1,6 +1,12 @@
 
 package BL;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Tools.DBconnection;
+
 /**
  * 
  * 
@@ -10,5 +16,37 @@ public class SessionManagerDB extends BL.SessionManager {
  * 
  * 
  */
-    public Tools.DBconnection dBconnection;
+    public Tools.DBconnection dbConnection;
+    
+    
+    public SessionManagerDB(){
+    	dbConnection = new DBconnection();
+    	Statement statement = dbConnection.getState();
+    	
+    	/* Exécution d'une requête de lecture */
+    	try {
+			ResultSet resultat = statement.executeQuery( "SELECT mail, password, username  FROM USER;" );
+		
+			/* Récupération des données du résultat de la requête de lecture */
+			while ( resultat.next() ) {
+			    String userMail = resultat.getString( "mail" );
+			    String password = resultat.getString( "password" );
+			    String userName = resultat.getString( "username" );
+
+			    /* Traiter ici les valeurs récupérées. */
+			}
+			
+    	} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	/* Exécution d'une requête d'écriture */
+    	try {
+			int statut = statement.executeUpdate( "INSERT INTO Utilisateur (email, mot_de_passe, nom, date_inscription) VALUES ('jmarc@mail.fr', MD5('lavieestbelle78'), 'jean-marc', NOW());" );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
  }
