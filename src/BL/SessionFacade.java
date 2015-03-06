d3e69c1c53269cf9bc08aafbdf1554619a76a175
infinity34 <remy.tartiere@free.fr>
2015-03-06 10:00:06 +0100

    appel de la methode de SessionManager pour login de SessionFacade

--- src/BL/SessionFacade.java
+++ src/BL/SessionFacade.java
@@ -7,54 +7,66 @@ package BL;
  */
 public class SessionFacade {
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @param username 
- * @param password 
- * @return 
- */
-    public Boolean Login(String username, String password) {        
-        // your code here
-        return null;
-    } 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean Logout() {        
-        // your code here
-        return null;
-    } 
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Data.User GetCurrentUser() {        
-        // your code here
-        return null;
-    } 
+	/**
+	 * 
+	 * 
+	 */
+	public BL.SessionManager sessionManager;
 
-/**
- * <p>Does ...</p>
- * 
- * 
- * @return 
- */
-    public Boolean Registration() {        
-        // your code here
-        return null;
-    } 
-/**
- * 
- * 
- */
-    public BL.SessionManager sessionManager;
- }
+	/**
+	 * 
+	 * 
+	 */
+	public SessionFacade(){
+		
+	}
+	
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @param username 
+	 * @param password 
+	 * @return 
+	 */
+	public Boolean Login(String username, String password) {        
+		// your code here
+		return sessionManager.Login(username,password);
+	} 
+
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Boolean Logout() {        
+		// your code here
+		return null;
+	} 
+
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Data.User GetCurrentUser() {        
+		// your code here
+		return null;
+	} 
+
+	/**
+	 * <p>Does ...</p>
+	 * 
+	 * 
+	 * @return 
+	 */
+	public Boolean Registration() {        
+		// your code here
+		return null;
+	} 
+
+}
