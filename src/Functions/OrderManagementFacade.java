6487cd5d3374ce85d91897fb7399840cd6942523
infinity34 <remy@192.168.0.22>
2015-03-15 12:53:53 +0100

    Ajout facade pour orderManagement

---
+++ src/Functions/OrderManagementFacade.java
@@ -0,0 +1,31 @@
+
+package Functions;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im26623am14bf96eff7cmm7af6]
+ */
+public class OrderManagementFacade {
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I610a0d25m14bffb8994amm71af]
+ */
+    public void cancelOrder() {        
+        // your code here
+    } 
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im26623am14bf96eff7cmm7aaf]
+ */
+    public Bl.OrderManager orderManager;
+/**
+ * 
+ * 
+ * @poseidon-object-id [Ima570141m14bf9c09221mm694d]
+ */
+    public Bl.SessionManager sessionManager;
+ }
