c02c030fa05f98633bcd41a814699710201f1ad1
Lyvia <adieupolytech@iMac-de-Fabien.local>
2015-03-16 12:32:31 +0100

    activityfacade

---
+++ src/Functions/ActivityFacade.java
@@ -0,0 +1,65 @@
+
+package Functions;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm5f3f]
+ */
+public class ActivityFacade {
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm5d82]
+ * @param name 
+ * @param inCharge 
+ * @param shortDescription 
+ * @param longDescription 
+ * @return 
+ */
+    public Data.Activity createActivity(String name, Persistence.inCharge inCharge, String shortDescription, String longDescription) {        
+        // your code here
+        return null;
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm5b94]
+ * @param name 
+ * @param inCharge 
+ * @param shortDescription 
+ * @param longDescription 
+ */
+    public void editActivity(String name, Data.InCharge inCharge, String shortDescription, String longDescription) {        
+        // your code here
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm5ab4]
+ * @param name 
+ */
+    public void deleteActivity(String name) {        
+        // your code here
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm59ad]
+ * @return 
+ */
+    public Persistence.Array getAllActivity() {        
+        // your code here
+        return null;
+    } 
+/**
+ * 
+ * 
+ * @poseidon-object-id [I3abaef47m14c1a7d7d62mm5ebf]
+ */
+    public Persistence.ActivityManager activityManager;
+ }
