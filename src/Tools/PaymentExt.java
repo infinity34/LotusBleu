08cff105ef7c0646a62ebcd10079c05aaea409bf
clement roque <clement.eberoque@gmail.com>
2015-03-16 18:11:14 +0100

    Ajout de Payment avec début IGraphique

---
+++ src/Tools/PaymentExt.java
@@ -0,0 +1,19 @@
+
+package Tools;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im3d5f9a4em14c178ccc5bmm7e00]
+ */
+public class PaymentExt {
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [Im3d5f9a4em14c178ccc5bmm7d15]
+ */
+    public void Paid() {        
+        // your code here
+    } 
+ }
