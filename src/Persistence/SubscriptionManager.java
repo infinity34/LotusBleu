fd2b99b5122af36253494fb9da1bd6d6b69985e1
clement roque <clement.eberoque@gmail.com>
2015-03-19 16:49:27 +0100

    Ajout class pour la subscription

---
+++ src/Persistence/SubscriptionManager.java
@@ -0,0 +1,52 @@
+
+package Persistence;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im5bf1e98dm14c1e0d6668mm7dd5]
+ */
+public class SubscriptionManager {
+
+/**
+ * <p>Represents ...</p>
+ * 
+ * @poseidon-object-id [Im1fefd7f2m14c220ff2f4mm7bae]
+ */
+    private float amount;
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im1fefd7f2m14c220ff2f4mm7d1a]
+ */
+    public UI.PaymentGUI paymentGUI;
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [Im1fefd7f2m14c220ff2f4mm7b9d]
+ * @return 
+ */
+    public Boolean ProceedSubscription() {        
+        // your code here
+        return null;
+    } 
+
+/**
+ * <p>Does ...</p>
+ * 
+ * @poseidon-object-id [Im1fefd7f2m14c220ff2f4mm7b78]
+ * @param amount 
+ * @return 
+ */
+    public Boolean ProceedPayment(float amount) {        
+        // your code here
+        return null;
+    } 
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im5bf1e98dm14c1e0d6668mm7d7b]
+ */
+    public Tools.BDConnection bDConnection;
+ }
---
+++ src/Persistence/card.java
@@ -0,0 +1,10 @@
+
+package UI;
+
+/**
+ * 
+ * 
+ * @poseidon-object-id [Im1fefd7f2m14c220ff2f4mm7c51]
+ */
+public class card {
+ }
