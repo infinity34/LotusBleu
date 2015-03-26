dfecb97d3e3e0cd675ee5a9a6525a7e58d054980
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-03-26 16:34:23 +0100

    admin managerDB

---
+++ src/Persistence/RegistrationAdminManagerDB.java
@@ -0,0 +1,36 @@
+
+package Persistence;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Data.ClassRoom;
+import Tools.DBconnection;
+
+/**
+ * 
+ */
+public class RegistrationAdminManagerDB extends RegistrationAdminManager {
+
+	/**
+	 * Tools allows the connection to the dataBase
+	 */
+	public DBconnection dBconnection;
+
+	/**
+	 * 
+	 */
+	public RegistrationAdminManagerDB() {
+		super();
+		this.dBconnection = DBconnection.getConnection();
+	}
+
+	/**
+	 * 
+	 */
+	public void loadRegistrationAdmins() {
+
+	}
+
+
+}
