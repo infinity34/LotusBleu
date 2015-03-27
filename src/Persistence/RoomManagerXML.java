f929de6bd88837341581bfb1009b3f1f701f9346
infinity34 <remy@192.168.0.22>
2015-03-27 19:47:49 +0100

    RoomManagement

--- src/Persistence/RoomManagerXML.java
+++ src/Persistence/RoomManagerXML.java
@@ -9,13 +9,13 @@ import Data.Room;
 public class RoomManagerXML extends Persistence.RoomManager {
 
 	@Override
-	public void addRoom(int roomID, int area, int numberOfParticipant) {
+	public void addRoom(String roomName, int area, int numberOfParticipant) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void addRoom(int roomID, int area) {
+	public void addRoom(String roomName, int area) {
 		// TODO Auto-generated method stub
 		
 	}
