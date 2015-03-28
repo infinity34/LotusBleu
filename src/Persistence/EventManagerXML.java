6fc1d50f7d00ef5df3be966c4049cb70afd98af7
infinity34 <remy@192.168.0.22>
2015-03-28 15:52:18 +0100

    Conflict ps ne pas me markasmerged sur le dossier en général

--- src/Persistence/EventManagerXML.java
+++ src/Persistence/EventManagerXML.java
@@ -3,7 +3,6 @@ package Persistence;
 
 import java.util.ArrayList;
 
-import Data.Activity;
 import Data.Event;
 import Data.TimeSlot;
 
@@ -27,7 +26,6 @@ public class EventManagerXML extends Persistence.EventManager {
 		
 	}
 
-	@Override
 	public Event getAnEventWithName(String name) {
 		return null;
 		// TODO Auto-generated method stub
@@ -61,5 +59,35 @@ public class EventManagerXML extends Persistence.EventManager {
 	public ArrayList<Event> getEventsForUser() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public ArrayList<Event> getEventsWithAName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Event getAnEventWithID(int evenIDreceived) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Event> getMyEvents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void registerToEvent(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelRegistrationToEvent(Event event) {
+		// TODO Auto-generated method stub
+		
 	}
  }
