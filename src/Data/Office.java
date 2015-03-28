be99c4e91f923a6d95883d78f570148a0d024781
infinity34 <remy@192.168.0.22>
2015-03-28 12:08:57 +0100

    javadoc

--- src/Data/Office.java
+++ src/Data/Office.java
@@ -1,7 +1,18 @@
 package Data;
-
+/**
+ * 
+ * @author remy
+ * @Design Prisca
+ */
 public class Office extends Room{
 	
+	/**
+	 * Constructor of a office
+	 * The office is defined by the area of the office
+	 * end this name
+	 * @param name
+	 * @param area
+	 */
 	public Office (String name, int area){
 		super(name, area);
 	}
--- src/Data/Room.java
+++ src/Data/Room.java
@@ -3,31 +3,63 @@ package Data;
 
 /**
  * 
- * 
- * @poseidon-object-id [Im37e90f00m14bf8ad3fd2mm76c2]
+ * @author remy
+ * @Design Prisca
+ *
  */
 public abstract class Room {
 	
+	/**
+	 * this is the area of the room (in scare meters)
+	 */
 	private int area;
+	
+	/**
+	 * this is the name of the room
+	 */
 	private String name;
 	
+	/**
+	 * Constructor of a room
+	 * A room can't be instantiated
+	 * @param name
+	 * @param area
+	 */
 	public Room(String name , int area){
 		this.area = area;
 		this.name = name;
 	}
 	
+	/**
+	 * set the area of the room 
+	 * with the area in parameter
+	 * @param area
+	 */
 	public void setArea(int area){
 		this.area = area;
 	}
 	
+	/**
+	 * 
+	 * @return the area of the room
+	 */
 	public int getArea(){
 		return this.area;
 	}
 
+	/**
+	 * 
+	 * @return the name of the room
+	 */
 	public String getRoomName() {
 		return name;
 	}
-
+	
+	/**
+	 * set the name of the room 
+	 * with the name in parameter
+	 * @param name
+	 */
 	public void setRoomName(String name) {
 		this.name = name;
 	}
