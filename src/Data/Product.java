8466a662265069748c5c7239e72e1ba595e65356
infinity34 <remy@192.168.0.22>
2015-03-28 14:20:07 +0100

    product

--- src/Data/Product.java
+++ src/Data/Product.java
@@ -3,14 +3,94 @@ package Data;
 
 /**
  * 
- * 
- * @poseidon-object-id [Ifc4ab2m14bf8c18020mm7556]
+ * @author remy
+ *
  */
 public class Product {
 	
+	private int productID;
+	private String productName;
+	private double productPrice;
+	private double productDiscount;
+	private int productQuantity;
+	private  Category categoryProduct;
 	
-	
-	public double getPrice(){
-		
+	/**
+	 * @return the product name
+	 */
+	public String getProductName() {
+		return productName;
 	}
+	
+	/**
+	 * Set the product name with the parameter productName
+	 * @param productName
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	/**
+	 * @return the product price
+	 */
+	public double getProductPrice() {
+		return productPrice;
+	}
+	
+	/**
+	 * set the product price with the argument
+	 * @param productPrice
+	 */
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	/**
+	 * @return the product discount
+	 */
+	public double getProductDiscount() {
+		return productDiscount;
+	}
+	
+	/**
+	 * set the Discount
+	 * @param productDiscount
+	 */
+	public void setProductDiscount(double productDiscount) {
+		this.productDiscount = productDiscount;
+	}
+	
+	/**
+	 * @return the product Quantity
+	 */
+	public int getProductQuantity() {
+		return productQuantity;
+	}
+	
+	
+	/**
+	 * set the product quantity with the 
+	 * quantity in parameter
+	 * @param productQuantity
+	 */
+	public void setProductQuantity(int productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+	
+	/**
+	 * @return the product category 
+	 */
+	public Category getCategoryProduct() {
+		return categoryProduct;
+	}
+	
+	/**
+	 * set the Category of the product with the
+	 * category in parameter
+	 * @param categoryProduct
+	 */
+	public void setCategoryProduct(Category categoryProduct) {
+		this.categoryProduct = categoryProduct;
+	}
+	
  }
