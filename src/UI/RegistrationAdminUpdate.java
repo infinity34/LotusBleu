a02571679d9ded32c28520f9d60fe5c4ecef634a
infinity34 <remy@192.168.0.22>
2015-03-29 16:33:00 +0200

    javadoc admin update

--- src/UI/RegistrationAdminUpdate.java
+++ src/UI/RegistrationAdminUpdate.java
@@ -18,6 +18,10 @@ public class RegistrationAdminUpdate extends JPanel {
 	private JTextField usermailTextfield;
 	/**
 	 * Create the panel.
+	 * this panel allows to modify a registration
+	 * the facade allows to access the different function needed
+	 * 
+	 * @param registrationAdminFacade
 	 */
 	public RegistrationAdminUpdate(RegistrationAdminFacade registrationAdminFacade) {
 
@@ -41,11 +45,11 @@ public class RegistrationAdminUpdate extends JPanel {
 		springLayout.putConstraint(SpringLayout.WEST, btnSearchUser, 145, SpringLayout.WEST, this);
 		add(btnSearchUser);
 		
+		/*search the user and display the other panel to update field */
 		btnSearchUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String usermail = usermailTextfield.getText();
 				User user = registrationAdminFacade.getRegistration(usermail);
-				//TODO affichage update
 				MainFrame.getMainFrame().setMainPanel(new RegistrationAdminUpdateField(user, registrationAdminFacade));
 			}
 		});
