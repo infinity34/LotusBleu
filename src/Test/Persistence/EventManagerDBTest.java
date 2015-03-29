2dc7dd60bf2fde9c9a630f1a17c05cf7762dc2a9
infinity34 <remy@192.168.0.22>
2015-03-29 19:59:45 +0200

    le bug qui tue est térassé (ContributorID dans Event)

---
+++ src/Test/Persistence/EventManagerDBTest.java
@@ -0,0 +1,55 @@
+package Persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Data.Event;
+import Data.TimeSlot;
+
+/**
+ * test on the classe EventManagerDB
+ * @author remy
+ *
+ */
+
+public class EventManagerDBTest {
+	
+	private static EventManager eventManager;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		eventManager = new EventManagerDB();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test //intégration
+	public void testUpdateEvent() {
+		Event eventToUpdate = this.eventManager.getAnEventWithID(6);
+		String eventName = "Fitness_modifié";
+		int roomID = 2;
+		TimeSlot eventTimeSlot = eventToUpdate.getEventTimeSlot();
+		String eventActivity = "Yoga";
+		String eventContributorName = "";
+		String eventContributorFirstname = "";
+		
+		this.eventManager.updateEvent(eventToUpdate, eventName, roomID, eventTimeSlot,
+				eventActivity, eventContributorName, eventContributorFirstname);
+	}
+
+}
