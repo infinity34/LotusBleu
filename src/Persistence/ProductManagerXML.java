1b6065424b3e6cd7d185ca399dfb7a78fe609051
infinity34 <remy@192.168.0.22>
2015-03-29 15:37:15 +0200

    rajout des méthodes dans les fichiers XML

--- src/Persistence/ProductManagerXML.java
+++ src/Persistence/ProductManagerXML.java
@@ -1,10 +1,53 @@
 
 package Persistence;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Data.Product;
+
 /**
  * 
  * 
  * @poseidon-object-id [I2d1a4d66m14c0966aad0mm7666]
  */
 public class ProductManagerXML extends Persistence.ProductManager {
+
+	@Override
+	public boolean addProduct(String name, float price, float discount,
+			int quantity, String categoryName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProduct(int id, String nameSel, float priceSel,
+			float discountSel, int quantitySel, String motherCategory) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteProduct(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Product> getAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getProduct(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultSet allProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
  }
--- src/Persistence/ShoppingManagerXML.java
+++ src/Persistence/ShoppingManagerXML.java
@@ -1,10 +1,33 @@
 
 package Persistence;
 
+import java.util.ArrayList;
+
+import Data.Category;
+import Data.Product;
+
 /**
  * 
  * 
  * @poseidon-object-id [I777ab4eam14bf4e91384mm7b6b]
  */
 public class ShoppingManagerXML extends Persistence.ShoppingManager {
+
+	@Override
+	public ArrayList<Category> getMainCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Category> getSubCategories(String parentCat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Product> getProductsByCat(int category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
  }
